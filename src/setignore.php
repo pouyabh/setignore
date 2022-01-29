b1c18e8143cb3049d21e7e82d445219fe1a825a1
pouyabh <pouyabh1380@gmail.com>
2022-01-29 11:27:15 +0330

    V1.1

--- src/setignore.php
+++ src/setignore.php
@@ -6,7 +6,8 @@ class setignore
 {
     public static function SetIgnore()
     {
-        $data = file_get_contents(base_path() . '/packages/pouyabh/setignore/.gitignore');
+        $data = file_get_contents(base_path() . '/packages/pouyabh/setignore/ignores.txt');
         $data = file_put_contents(base_path() . '/.gitignore', $data);
     }
 }
+
